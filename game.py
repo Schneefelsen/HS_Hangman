dcf1ed676b90b36a74564e0d283c8cc832df3490
Schneefelsen <135202288+Schneefelsen@users.noreply.github.com>
2023-06-26 20:53:37 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -1,57 +1,85 @@
-import string
 import random
 
-def bot_func(number):
-    if number % 4 == 0:
-        return 3
-    elif (number + 1) % 4 == 0:
-        return 2
-    elif (number + 2) % 4 == 0:
-        return 1
-    elif 1 < number < 4:
-        return number - 1
-    elif number == 1:
-        return 1
-    else:
-        return int(random.uniform(1, 3.99))
 
-pencils = input("How many pencils would you like to use:\n")
-names = ["John", "Bot"]
-permitted_num = ["1", "2", "3"]
+class RockPaperScissors:
 
-while True:
-    for letter in pencils:
-        if letter not in string.digits:
-            pencils = input("The number of pencils should be numeric\n")
-            break
-    else:
-        pencils = int(pencils)
-        if pencils == 0:
-            pencils = input("The number of pencils should be positive\n")
-            continue
-        break
+    def __init__(self):
+        self.hands = {"rock": [0, -1, 1], "paper": [1, 0, -1], "scissors": [-1, 1, 0]}
+        self.name = ""
+        self.score = 0
+        self.known_user = False
 
-name = input(f"Who will be the first ({names[0]}, {names[1]}):\n")
+    def check_name(self):
+        rating = open("rating.txt", "r")
+        for line in rating:
+            if line.startswith(self.name):
+                self.score = int(line.split()[1])
+                self.known_user = True
+                break
+        rating.close()
 
-while pencils != 0:
-    if name not in names:
-        name = input(f"Choose between {names[0]} and {names[1]}\n")
-        continue
-    pencils_str = "|" * pencils
-    print(pencils_str)
-    print(f"{name}'s turn:\n")
-    if name == names[0]:
-        takeaway = input()
-        while takeaway not in permitted_num:
-            takeaway = input("Possible values: '1', '2' or '3'\n")
-        while pencils - int(takeaway) < 0:
-            takeaway = input("Too many pencils were taken")
-        name = names[1]
+    def update_score(self):
+        if self.known_user:
+            rating = open("rating.txt", "r+")
+            ratings = rating.readlines()
+            for num, elem in enumerate(ratings):
+                if elem.startswith(self.name):
+                    ratings[num] = f"{self.name} {self.score}"
+            rating.writelines(ratings)
+        else:
+            rating = open("rating.txt", "a")
+            print(f"{self.name} {self.score}", file=rating)
+        rating.close()
 
-    else:
-        takeaway = bot_func(pencils)
-        print(takeaway)
-        name = names[0]
+    def update_hands(self, options):
+        option_dict = {}
+        if options != [""]:
+            weak_strong_list = [0]
+            for _ in range(len(options) // 2):
+                weak_strong_list.insert(1, -1)
+                weak_strong_list.append(1)
+            if len(options) % 2:
+                weak_strong_list.pop()
+            for pos, option in enumerate(options):
+                option_dict.update({option: weak_strong_list.copy()})
+                weak_strong_list.insert(0, weak_strong_list.pop())
+            self.hands = option_dict
 
-    pencils = pencils - int(takeaway)
-print(f"{name} won!")
+    def get_result(self, player, computer):
+        computer_position = list(self.hands.keys()).index(computer)
+        if self.hands[player][computer_position] == -1:
+            print(f"Sorry, but the computer chose {computer}")
+        elif self.hands[player][computer_position] == 1:
+            print(f"Well done. The computer chose {computer} and failed")
+            self.score += 100
+        else:
+            print(f"There is a draw {computer}")
+            self.score += 50
+
+    def play(self):
+        self.name = input("Enter your name: ")
+        self.check_name()
+        print(f"Hello, {self.name}")
+        wanted_hands = input().split(",")
+        self.update_hands(wanted_hands)
+        print("Okay, let's start")
+        while True:
+            possible_hands = list(self.hands.keys())
+            com_hand = random.choice(possible_hands)
+            user_hand = input()
+            try:
+                if user_hand == "!exit":
+                    print("Bye!")
+                    self.update_score()
+                    break
+                elif user_hand == "!rating":
+                    print(f"Your rating: {self.score}")
+                else:
+                    self.get_result(user_hand, com_hand)
+            except KeyError:
+                print("Invalid input")
+
+
+if __name__ == "__main__":
+    game = RockPaperScissors()
+    game.play()
